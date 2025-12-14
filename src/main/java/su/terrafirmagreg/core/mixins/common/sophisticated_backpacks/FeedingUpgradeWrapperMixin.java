6a2800672c0444d3ca776e42d4aef396d040f149
Mqrius <Nuntius.Marii@Gmail.com>
2025-12-14 02:04:34 +0100

    Add Pseudo to FeedingUpgradeWrapperMixin
    Signed-off-by: Mqrius <Nuntius.Marii@Gmail.com>

--- src/main/java/su/terrafirmagreg/core/mixins/common/sophisticated_backpacks/FeedingUpgradeWrapperMixin.java
+++ src/main/java/su/terrafirmagreg/core/mixins/common/sophisticated_backpacks/FeedingUpgradeWrapperMixin.java
@@ -3,6 +3,7 @@ package su.terrafirmagreg.core.mixins.common.sophisticatedbackpacks;
 import javax.annotation.Nullable;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,12 +13,11 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
-import net.p3pp3rf1y.sophisticatedcore.upgrades.feeding.FeedingUpgradeWrapper;
-
 // Cancel backpack feeding upgrade tick when player is in PlayerRevive bleeding state
 // This prevents food from being consumed without effect
 
-@Mixin(value = FeedingUpgradeWrapper.class, remap = false)
+@Pseudo
+@Mixin(targets = "net.p3pp3rf1y.sophisticatedcore.upgrades.feeding.FeedingUpgradeWrapper", remap = false)
 public class FeedingUpgradeWrapperMixin {
     @Inject(method = "tick", at = @At("HEAD"), remap = false, cancellable = true)
     private void tfg$preventFeedingWhenBleeding(@Nullable Entity entity, Level level, BlockPos pos, CallbackInfo ci) {
